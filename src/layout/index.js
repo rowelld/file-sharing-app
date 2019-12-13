089b76e0c5a4431c30efe4bc5762d235249a5242
Rowell Niño Dinolan <rdinolan@spinginemail.com>
2019-12-13 17:53:14 +0800

    My first commit>

---
+++ src/layout/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Home from '../pages/home'
+
+class Layout extends Component {
+    render() {
+        return (<div className={"app-layout"} >
+            <Home />
+        </div>)
+    }
+}
+
+export default Layout;
